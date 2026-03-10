183752480c1270375915e5674ce89fcb2ce6d6ff
theincredibleplayer <54246318+theincredibleplayer@users.noreply.github.com>
2026-03-10 04:15:17 +0200

    hook int64, early color detection

--- src/hook.rs
+++ src/hook.rs
@@ -19,8 +19,10 @@ const RETURN_ORIGINAL: u64 = 2;
 
 const INVALID_COLOR: i32 = -2;
 
+static INT64_OFFSET: usize = 0x4e53b0; // 13.0.3
 static INT_OFFSET: usize = 0x4e53a0; // 13.0.3
 static FLOAT_OFFSET: usize = 0x4e53e0; // 13.0.3
+static SET_ID_EARLY: usize = 0x6079d0; // 13.0.3
 static ARTICLE_OFFSET: usize = 0x3a6670; // 13.0.3
 static COPY_RESET_OFFSET: usize = 0xb96770;
 
@@ -68,6 +70,50 @@ unsafe fn get_color(module_accessor: &mut BattleObjectModuleAccessor) -> i32 {
 
     return color;
 }
+
+
+#[skyline::hook(offset = SET_ID_EARLY)]
+pub unsafe extern "C" fn set_entry_id_early(fighter: &mut Fighter, battle_object_id: u32, kind: i32, entry_id: i32, hash: smash::phx::Hash40) {
+    let module_accessor = fighter.battle_object.module_accessor;
+    WorkModule::set_int(module_accessor,entry_id, *FIGHTER_INSTANCE_WORK_ID_INT_ENTRY_ID);
+    original!()(fighter, battle_object_id, kind, entry_id,hash)
+}
+
+
+#[skyline::hook(offset=INT64_OFFSET)]
+pub unsafe fn get_param_int_64_hook(module: u64, param_type: u64, param_hash: u64) -> i64 {
+    let original_value = original!()(module, param_type, param_hash);
+    let mut module_accessor = *((module as *mut u64).offset(1)) as *mut BattleObjectModuleAccessor;
+    let module_accessor_reference = &mut *module_accessor;
+    let id = WorkModule::get_int(module_accessor, *FIGHTER_INSTANCE_WORK_ID_INT_ENTRY_ID) as usize;
+    let mut slot = get_color(&mut *module_accessor);
+    if slot == INVALID_COLOR { return original_value; }
+
+    let mut fighter_kind = utility::get_kind(module_accessor_reference);
+    if utility::get_category(module_accessor_reference) == *BATTLE_OBJECT_CATEGORY_WEAPON {
+        fighter_kind *= -1;
+        /*
+        let owner_id = WorkModule::get_int(module_accessor, *WEAPON_INSTANCE_WORK_ID_INT_ACTIVATE_FOUNDER_ID) as u32;
+        if sv_battle_object::is_active(owner_id) {
+            slot = WorkModule::get_int(sv_battle_object::module_accessor(owner_id), *FIGHTER_INSTANCE_WORK_ID_INT_COLOR);
+        }
+        */
+    }
+
+    if FighterParamModule::has_kind(fighter_kind)
+    {
+        if let Some(mult) = FighterParamModule::get_int_param_mul(fighter_kind, slot,param_type, param_hash){
+            let temp = (original_value as f32) * mult;
+            return temp as i64;
+        }
+        else if let Some(new_param) = FighterParamModule::get_int_param(fighter_kind, slot,param_type, param_hash){
+            return new_param as i64;
+        }
+    }
+
+    return original_value;
+}
+
 
 #[skyline::hook(offset=INT_OFFSET)]
 pub unsafe fn get_param_int_hook(module: u64, param_type: u64, param_hash: u64) -> i32 {
@@ -396,8 +442,10 @@ pub fn install_params() {
     if super::can_hook_params() {
         println!("[libparam_config] Hooking GetParam functions");
         skyline::install_hooks!(
+            set_entry_id_early,
             get_param_int_hook,
             get_param_float_hook,
+            get_param_int_64_hook,
             //read_melee_mode
         );
         *super::IS_HOOKED_PARAMS.write() = true;
